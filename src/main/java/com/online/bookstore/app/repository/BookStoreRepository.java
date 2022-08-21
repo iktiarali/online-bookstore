1a817704fc97a560f68fd1ee9085d27d4b94a2cd
iktiarali <108665994+iktiarali@users.noreply.github.com>
2022-08-22 01:43:23 +0530

    Update BookStoreRepository.java

--- src/main/java/com/online/bookstore/app/repository/BookStoreRepository.java
+++ src/main/java/com/online/bookstore/app/repository/BookStoreRepository.java
@@ -1,9 +1,10 @@
 package com.online.bookstore.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.online.bookstore.app.entity.Book;
 
+@Repository
 public interface BookStoreRepository extends JpaRepository<Book, Long>{
 	
 }
